7efbc4970097bf39627c3dcfcb093506ffa78b54
shidecai <shidecai@meituan.com>
2025-09-24 11:39:53 +0800

    注释

--- common/src/main/java/org/apache/rocketmq/common/namesrv/NamesrvConfig.java
+++ common/src/main/java/org/apache/rocketmq/common/namesrv/NamesrvConfig.java
@@ -23,77 +23,211 @@ package org.apache.rocketmq.common.namesrv;
 import java.io.File;
 import org.apache.rocketmq.common.MixAll;
 
+/**
+ * RocketMQ NameServer 配置类
+ *
+ * <p>作用和职责：</p>
+ * <ul>
+ *   <li><b>配置管理</b>：管理NameServer运行时的各种配置参数</li>
+ *   <li><b>性能调优</b>：提供线程池、队列容量等性能相关配置</li>
+ *   <li><b>功能开关</b>：控制NameServer的各种功能特性开关</li>
+ *   <li><b>路径配置</b>：管理配置文件、数据文件的存储路径</li>
+ *   <li><b>集群配置</b>：支持集群模式和高可用相关配置</li>
+ * </ul>
+ *
+ * <p>主要配置分类：</p>
+ * <ul>
+ *   <li><b>基础配置</b>：RocketMQ安装目录、环境名称等</li>
+ *   <li><b>存储配置</b>：配置文件路径、KV存储路径等</li>
+ *   <li><b>线程池配置</b>：处理客户端和Broker请求的线程池参数</li>
+ *   <li><b>功能开关</b>：顺序消息、Topic列表、Controller等功能开关</li>
+ *   <li><b>高可用配置</b>：主从切换、故障检测等高可用特性</li>
+ * </ul>
+ *
+ * <p>配置文件示例：</p>
+ * <pre>
+ * # NameServer基础配置
+ * rocketmqHome=/opt/rocketmq
+ * productEnvName=production
+ *
+ * # 线程池配置
+ * clientRequestThreadPoolNums=16
+ * defaultThreadPoolNums=32
+ *
+ * # 功能开关
+ * orderMessageEnable=true
+ * enableControllerInNamesrv=true
+ * </pre>
+ *
+ * @author RocketMQ Team
+ * @since 3.0.0
+ */
 public class NamesrvConfig {
 
+    /**
+     * RocketMQ安装根目录
+     * <p>用于定位RocketMQ的安装路径，影响日志、配置文件等的默认位置</p>
+     * <p>默认值：从系统属性ROCKETMQ_HOME获取</p>
+     */
     private String rocketmqHome = MixAll.ROCKETMQ_HOME_DIR;
+
+    /**
+     * KV配置存储路径
+     * <p>用于存储NameServer的键值对配置信息，如Topic配置、Broker信息等</p>
+     * <p>默认值：${user.home}/namesrv/kvConfig.json</p>
+     */
     private String kvConfigPath = System.getProperty("user.home") + File.separator + "namesrv" + File.separator + "kvConfig.json";
+
+    /**
+     * NameServer配置文件存储路径
+     * <p>用于持久化NameServer的配置信息</p>
+     * <p>默认值：${user.home}/namesrv/namesrv.properties</p>
+     */
     private String configStorePath = System.getProperty("user.home") + File.separator + "namesrv" + File.separator + "namesrv.properties";
+
+    /**
+     * 产品环境名称
+     * <p>用于标识当前运行环境，可用于环境隔离和配置区分</p>
+     * <p>默认值：center</p>
+     */
     private String productEnvName = "center";
+
+    /**
+     * 集群测试模式开关
+     * <p>是否启用集群测试模式，测试模式下可能会有不同的行为</p>
+     * <p>默认值：false</p>
+     */
     private boolean clusterTest = false;
+
+    /**
+     * 顺序消息功能开关
+     * <p>是否启用顺序消息功能，影响Topic路由和消息队列的处理</p>
+     * <p>默认值：false</p>
+     */
     private boolean orderMessageEnable = false;
+
+    /**
+     * 是否向Broker返回顺序Topic配置
+     * <p>控制NameServer是否将顺序Topic的配置信息返回给Broker</p>
+     * <p>默认值：true</p>
+     */
     private boolean returnOrderTopicConfigToBroker = true;
 
     /**
-     * Indicates the nums of thread to handle client requests, like GET_ROUTEINTO_BY_TOPIC.
+     * 客户端请求处理线程池大小
+     * <p>处理客户端请求（如GET_ROUTEINTO_BY_TOPIC）的线程数量</p>
+     * <p>默认值：8</p>
      */
     private int clientRequestThreadPoolNums = 8;
+
     /**
-     * Indicates the nums of thread to handle broker or operation requests, like REGISTER_BROKER.
+     * 默认请求处理线程池大小
+     * <p>处理Broker注册等操作请求（如REGISTER_BROKER）的线程数量</p>
+     * <p>默认值：16</p>
      */
     private int defaultThreadPoolNums = 16;
+
     /**
-     * Indicates the capacity of queue to hold client requests.
+     * 客户端请求队列容量
+     * <p>客户端请求处理队列的最大容量，超过此容量的请求将被拒绝</p>
+     * <p>默认值：50000</p>
      */
     private int clientRequestThreadPoolQueueCapacity = 50000;
+
     /**
-     * Indicates the capacity of queue to hold broker or operation requests.
+     * 默认请求队列容量
+     * <p>Broker和操作请求处理队列的最大容量</p>
+     * <p>默认值：10000</p>
      */
     private int defaultThreadPoolQueueCapacity = 10000;
+
     /**
-     * Interval of periodic scanning for non-active broker;
+     * 扫描非活跃Broker的时间间隔（毫秒）
+     * <p>NameServer定期扫描并清理非活跃Broker的时间间隔</p>
+     * <p>默认值：5000毫秒（5秒）</p>
      */
     private long scanNotActiveBrokerInterval = 5 * 1000;
 
+    /**
+     * Broker注销队列容量
+     * <p>处理Broker注销请求的队列容量</p>
+     * <p>默认值：3000</p>
+     */
     private int unRegisterBrokerQueueCapacity = 3000;
 
     /**
-     * Support acting master or not.
-     *
-     * The slave can be an acting master when master node is down to support following operations:
-     * 1. support lock/unlock message queue operation.
-     * 2. support searchOffset, query maxOffset/minOffset operation.
-     * 3. support query earliest msg store time.
+     * 是否支持代理Master功能
+     * <p>当Master节点宕机时，Slave是否可以作为代理Master提供以下服务：</p>
+     * <ul>
+     *   <li>支持消息队列的锁定/解锁操作</li>
+     *   <li>支持searchOffset、查询maxOffset/minOffset操作</li>
+     *   <li>支持查询最早消息存储时间</li>
+     * </ul>
+     * <p>默认值：false</p>
      */
     private boolean supportActingMaster = false;
 
+    /**
+     * 是否启用所有Topic列表功能
+     * <p>控制是否允许获取所有Topic的列表信息</p>
+     * <p>默认值：true</p>
+     */
     private volatile boolean enableAllTopicList = true;
 
-
+    /**
+     * 是否启用Topic列表功能
+     * <p>控制是否允许获取Topic列表信息</p>
+     * <p>默认值：true</p>
+     */
     private volatile boolean enableTopicList = true;
 
+    /**
+     * 是否通知最小BrokerId变更
+     * <p>当集群中最小BrokerId发生变化时是否发送通知</p>
+     * <p>默认值：false</p>
+     */
     private volatile boolean notifyMinBrokerIdChanged = false;
 
     /**
-     * Is startup the controller in this name-srv
+     * 是否在NameServer中启用Controller
+     * <p>是否在当前NameServer实例中启动Controller管理器</p>
+     * <p>Controller用于管理Broker的主从切换和故障转移</p>
+     * <p>默认值：false</p>
      */
     private boolean enableControllerInNamesrv = false;
 
+    /**
+     * 是否需要等待服务就绪
+     * <p>启动时是否需要等待相关服务完全就绪后再对外提供服务</p>
+     * <p>默认值：false</p>
+     */
     private volatile boolean needWaitForService = false;
 
+    /**
+     * 等待服务就绪的时间（秒）
+     * <p>当needWaitForService为true时，等待服务就绪的最大时间</p>
+     * <p>默认值：45秒</p>
+     */
     private int waitSecondsForService = 45;
 
     /**
-     * If enable this flag, the topics that don't exist in broker registration payload will be deleted from name server.
+     * 是否随Broker注册删除Topic
+     * <p>启用此标志后，Broker注册时不包含的Topic将从NameServer中删除</p>
      *
-     * WARNING:
-     * 1. Enable this flag and "enableSingleTopicRegister" of broker config meanwhile to avoid losing topic route info unexpectedly.
-     * 2. This flag does not support static topic currently.
+     * <p><b>警告：</b></p>
+     * <ul>
+     *   <li>启用此标志时，需同时启用Broker配置中的"enableSingleTopicRegister"以避免意外丢失Topic路由信息</li>
+     *   <li>此标志目前不支持静态Topic</li>
+     * </ul>
+     * <p>默认值：false</p>
      */
     private boolean deleteTopicWithBrokerRegistration = false;
+
     /**
-     * Config in this black list will be not allowed to update by command.
-     * Try to update this config black list by restart process.
-     * Try to update configures in black list by restart process.
+     * 配置黑名单
+     * <p>此黑名单中的配置项不允许通过命令进行更新，只能通过重启进程来更新</p>
+     * <p>多个配置项用分号分隔</p>
+     * <p>默认值：configBlackList;configStorePath;kvConfigPath</p>
      */
     private String configBlackList = "configBlackList;configStorePath;kvConfigPath";
 
--- namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvStartup.java
+++ namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvStartup.java
@@ -16,12 +16,6 @@
  */
 package org.apache.rocketmq.namesrv;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
-import java.util.concurrent.Callable;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
@@ -34,31 +28,103 @@ import org.apache.rocketmq.common.constant.LoggerName;
 import org.apache.rocketmq.common.namesrv.NamesrvConfig;
 import org.apache.rocketmq.controller.ControllerManager;
 import org.apache.rocketmq.logging.org.slf4j.Logger;
+import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
 import org.apache.rocketmq.remoting.netty.NettyClientConfig;
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
 import org.apache.rocketmq.remoting.protocol.RemotingCommand;
-import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
 import org.apache.rocketmq.srvutil.ServerUtil;
 import org.apache.rocketmq.srvutil.ShutdownHookThread;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+
+/**
+ * RocketMQ NameServer 启动类
+ *
+ * <p>作用和职责：</p>
+ * <ul>
+ *   <li><b>NameServer启动入口</b>：提供NameServer服务的主启动入口</li>
+ *   <li><b>配置管理</b>：解析命令行参数和配置文件，初始化各种配置对象</li>
+ *   <li><b>组件创建</b>：创建和初始化NamesrvController和ControllerManager</li>
+ *   <li><b>生命周期管理</b>：管理NameServer的启动、运行和关闭流程</li>
+ *   <li><b>优雅关闭</b>：注册JVM关闭钩子，确保服务优雅停止</li>
+ * </ul>
+ *
+ * <p>NameServer在RocketMQ中的作用：</p>
+ * <ul>
+ *   <li><b>路由注册中心</b>：管理Broker的路由信息，包括Topic和队列的分布</li>
+ *   <li><b>服务发现</b>：为生产者和消费者提供Broker地址查询服务</li>
+ *   <li><b>元数据管理</b>：存储和管理Topic配置、Broker状态等元数据</li>
+ *   <li><b>负载均衡支持</b>：为客户端提供Broker选择的基础数据</li>
+ * </ul>
+ *
+ * <p>启动流程：</p>
+ * <ol>
+ *   <li>解析命令行参数和配置文件</li>
+ *   <li>创建NamesrvController实例</li>
+ *   <li>初始化并启动NameServer服务</li>
+ *   <li>可选：启动ControllerManager（用于主从切换管理）</li>
+ *   <li>注册关闭钩子，等待服务运行</li>
+ * </ol>
+ *
+ * @author RocketMQ Team
+ * @since 3.0.0
+ */
 public class NamesrvStartup {
 
     private static final Logger log = LoggerFactory.getLogger(LoggerName.NAMESRV_LOGGER_NAME);
+
+    /** 控制台日志记录器，用于输出配置信息 */
     private static final Logger logConsole = LoggerFactory.getLogger(LoggerName.NAMESRV_CONSOLE_LOGGER_NAME);
+
+    /** 配置文件属性集合 */
     private static Properties properties = null;
+
+    /** NameServer 配置对象 */
     private static NamesrvConfig namesrvConfig = null;
+
+    /** Netty 服务端配置对象 */
     private static NettyServerConfig nettyServerConfig = null;
+
+    /** Netty 客户端配置对象 */
     private static NettyClientConfig nettyClientConfig = null;
+
+    /** 控制器配置对象（用于主从切换管理） */
     private static ControllerConfig controllerConfig = null;
 
+    /**
+     * NameServer 主启动方法
+     *
+     * <p>程序入口点，负责启动NameServer和可选的ControllerManager</p>
+     *
+     * @param args 命令行参数
+     *             -c configFile: 指定配置文件路径
+     *             -p: 打印所有配置项并退出
+     */
     public static void main(String[] args) {
+        // 启动 NameServer 核心服务
         main0(args);
+        // 启动 Controller 管理器（如果启用）
         controllerManagerMain();
     }
 
+    /**
+     * NameServer 核心启动方法
+     *
+     * <p>解析配置并创建启动NameServer控制器</p>
+     *
+     * @param args 命令行参数
+     * @return NamesrvController 实例，如果启动失败则返回null
+     */
     public static NamesrvController main0(String[] args) {
         try {
+            // 1. 解析命令行参数和配置文件
             parseCommandlineAndConfigFile(args);
+            // 2. 创建并启动 NameServer 控制器
             NamesrvController controller = createAndStartNamesrvController();
             return controller;
         } catch (Throwable e) {
@@ -69,6 +135,14 @@ public class NamesrvStartup {
         return null;
     }
 
+    /**
+     * ControllerManager 启动方法
+     *
+     * <p>如果启用了Controller功能，则创建并启动ControllerManager</p>
+     * <p>ControllerManager用于管理Broker的主从切换和故障转移</p>
+     *
+     * @return ControllerManager 实例，如果未启用或启动失败则返回null
+     */
     public static ControllerManager controllerManagerMain() {
         try {
             if (namesrvConfig.isEnableControllerInNamesrv()) {
@@ -81,9 +155,26 @@ public class NamesrvStartup {
         return null;
     }
 
+    /**
+     * 解析命令行参数和配置文件
+     *
+     * <p>功能包括：</p>
+     * <ul>
+     *   <li>设置RocketMQ版本信息</li>
+     *   <li>解析命令行选项（-c 配置文件路径，-p 打印配置）</li>
+     *   <li>加载配置文件并映射到配置对象</li>
+     *   <li>初始化各种配置对象（NamesrvConfig、NettyServerConfig等）</li>
+     *   <li>验证必要的环境变量（ROCKETMQ_HOME）</li>
+     * </ul>
+     *
+     * @param args 命令行参数数组
+     * @throws Exception 配置解析或文件读取异常
+     */
     public static void parseCommandlineAndConfigFile(String[] args) throws Exception {
+        // 设置 RocketMQ 版本信息到系统属性
         System.setProperty(RemotingCommand.REMOTING_VERSION_KEY, Integer.toString(MQVersion.CURRENT_VERSION));
 
+        // 构建命令行选项
         Options options = ServerUtil.buildCommandlineOptions(new Options());
         CommandLine commandLine = ServerUtil.parseCmdLine("mqnamesrv", args, buildCommandlineOptions(options), new DefaultParser());
         if (null == commandLine) {
@@ -91,19 +182,28 @@ public class NamesrvStartup {
             return;
         }
 
+        // 初始化配置对象
         namesrvConfig = new NamesrvConfig();
         nettyServerConfig = new NettyServerConfig();
         nettyClientConfig = new NettyClientConfig();
+        // 设置 NameServer 默认监听端口为 9876
         nettyServerConfig.setListenPort(9876);
+
+        // 处理配置文件选项 (-c)
         if (commandLine.hasOption('c')) {
             String file = commandLine.getOptionValue('c');
             if (file != null) {
+                // 读取配置文件
                 InputStream in = new BufferedInputStream(Files.newInputStream(Paths.get(file)));
                 properties = new Properties();
                 properties.load(in);
+
+                // 将配置文件属性映射到配置对象
                 MixAll.properties2Object(properties, namesrvConfig);
                 MixAll.properties2Object(properties, nettyServerConfig);
                 MixAll.properties2Object(properties, nettyClientConfig);
+
+                // 如果启用了 Controller 功能，初始化相关配置
                 if (namesrvConfig.isEnableControllerInNamesrv()) {
                     controllerConfig = new ControllerConfig();
                     JraftConfig jraftConfig = new JraftConfig();
@@ -111,14 +211,17 @@ public class NamesrvStartup {
                     MixAll.properties2Object(properties, controllerConfig);
                     MixAll.properties2Object(properties, jraftConfig);
                 }
-                namesrvConfig.setConfigStorePath(file);
 
+                namesrvConfig.setConfigStorePath(file);
                 System.out.printf("load config properties file OK, %s%n", file);
                 in.close();
             }
         }
 
+        // 将命令行参数映射到配置对象
         MixAll.properties2Object(ServerUtil.commandLine2Properties(commandLine), namesrvConfig);
+
+        // 处理打印配置选项 (-p)
         if (commandLine.hasOption('p')) {
             MixAll.printObjectProperties(logConsole, namesrvConfig);
             MixAll.printObjectProperties(logConsole, nettyServerConfig);
@@ -129,114 +232,207 @@ public class NamesrvStartup {
             System.exit(0);
         }
 
+        // 验证 ROCKETMQ_HOME 环境变量
         if (null == namesrvConfig.getRocketmqHome()) {
             System.out.printf("Please set the %s variable in your environment to match the location of the RocketMQ installation%n", MixAll.ROCKETMQ_HOME_ENV);
             System.exit(-2);
         }
+
+        // 打印最终配置信息到日志
         MixAll.printObjectProperties(log, namesrvConfig);
         MixAll.printObjectProperties(log, nettyServerConfig);
-
     }
 
+    /**
+     * 创建并启动 NameServer 控制器
+     *
+     * <p>完整的创建和启动流程，包括成功提示信息输出</p>
+     *
+     * @return 启动成功的 NamesrvController 实例
+     * @throws Exception 创建或启动过程中的异常
+     */
     public static NamesrvController createAndStartNamesrvController() throws Exception {
-
+        // 创建 NameServer 控制器
         NamesrvController controller = createNamesrvController();
+        // 启动控制器
         start(controller);
+
+        // 输出启动成功信息
         NettyServerConfig serverConfig = controller.getNettyServerConfig();
-        String tip = String.format("The Name Server boot success. serializeType=%s, address %s:%d", RemotingCommand.getSerializeTypeConfigInThisServer(), serverConfig.getBindAddress(), serverConfig.getListenPort());
+        String tip = String.format("The Name Server boot success. serializeType=%s, address %s:%d",
+            RemotingCommand.getSerializeTypeConfigInThisServer(),
+            serverConfig.getBindAddress(),
+            serverConfig.getListenPort());
         log.info(tip);
         System.out.printf("%s%n", tip);
         return controller;
     }
 
+    /**
+     * 创建 NameServer 控制器实例
+     *
+     * <p>使用解析好的配置对象创建NamesrvController，并注册配置信息</p>
+     *
+     * @return 新创建的 NamesrvController 实例
+     */
     public static NamesrvController createNamesrvController() {
-
+        // 创建 NameServer 控制器，传入各种配置对象
         final NamesrvController controller = new NamesrvController(namesrvConfig, nettyServerConfig, nettyClientConfig);
-        // remember all configs to prevent discard
+        // 注册配置属性，防止配置丢失
         controller.getConfiguration().registerConfig(properties);
         return controller;
     }
 
+    /**
+     * 启动 NameServer 控制器
+     *
+     * <p>启动流程：</p>
+     * <ol>
+     *   <li>初始化控制器</li>
+     *   <li>注册JVM关闭钩子</li>
+     *   <li>启动服务</li>
+     * </ol>
+     *
+     * @param controller 要启动的 NamesrvController 实例
+     * @return 启动后的控制器实例
+     * @throws Exception 初始化或启动异常
+     */
     public static NamesrvController start(final NamesrvController controller) throws Exception {
-
         if (null == controller) {
             throw new IllegalArgumentException("NamesrvController is null");
         }
 
+        // 初始化控制器
         boolean initResult = controller.initialize();
         if (!initResult) {
             controller.shutdown();
             System.exit(-3);
         }
 
+        // 注册JVM关闭钩子，确保优雅关闭
         Runtime.getRuntime().addShutdownHook(new ShutdownHookThread(log, (Callable<Void>) () -> {
             controller.shutdown();
             return null;
         }));
 
+        // 启动控制器服务
         controller.start();
 
         return controller;
     }
 
+    /**
+     * 创建并启动 Controller 管理器
+     *
+     * <p>ControllerManager用于管理Broker的主从切换和故障转移</p>
+     *
+     * @return 启动成功的 ControllerManager 实例
+     * @throws Exception 创建或启动过程中的异常
+     */
     public static ControllerManager createAndStartControllerManager() throws Exception {
         ControllerManager controllerManager = createControllerManager();
         start(controllerManager);
+
         String tip = "The ControllerManager boot success. serializeType=" + RemotingCommand.getSerializeTypeConfigInThisServer();
         log.info(tip);
         System.out.printf("%s%n", tip);
         return controllerManager;
     }
 
+    /**
+     * 创建 Controller 管理器实例
+     *
+     * @return 新创建的 ControllerManager 实例
+     * @throws Exception 创建过程中的异常
+     */
     public static ControllerManager createControllerManager() throws Exception {
+        // 克隆网络服务配置，避免配置冲突
         NettyServerConfig controllerNettyServerConfig = (NettyServerConfig) nettyServerConfig.clone();
         ControllerManager controllerManager = new ControllerManager(controllerConfig, controllerNettyServerConfig, nettyClientConfig);
-        // remember all configs to prevent discard
+        // 注册配置属性
         controllerManager.getConfiguration().registerConfig(properties);
         return controllerManager;
     }
 
+    /**
+     * 启动 Controller 管理器
+     *
+     * @param controllerManager 要启动的 ControllerManager 实例
+     * @return 启动后的管理器实例
+     * @throws Exception 初始化或启动异常
+     */
     public static ControllerManager start(final ControllerManager controllerManager) throws Exception {
-
         if (null == controllerManager) {
             throw new IllegalArgumentException("ControllerManager is null");
         }
 
+        // 初始化管理器
         boolean initResult = controllerManager.initialize();
         if (!initResult) {
             controllerManager.shutdown();
             System.exit(-3);
         }
 
+        // 注册关闭钩子
         Runtime.getRuntime().addShutdownHook(new ShutdownHookThread(log, (Callable<Void>) () -> {
             controllerManager.shutdown();
             return null;
         }));
 
+        // 启动管理器服务
         controllerManager.start();
 
         return controllerManager;
     }
 
+    /**
+     * 关闭 NameServer 控制器
+     *
+     * @param controller 要关闭的控制器实例
+     */
     public static void shutdown(final NamesrvController controller) {
         controller.shutdown();
     }
 
+    /**
+     * 关闭 Controller 管理器
+     *
+     * @param controllerManager 要关闭的管理器实例
+     */
     public static void shutdown(final ControllerManager controllerManager) {
         controllerManager.shutdown();
     }
 
+    /**
+     * 构建命令行选项
+     *
+     * <p>支持的选项：</p>
+     * <ul>
+     *   <li>-c configFile: 指定配置文件路径</li>
+     *   <li>-p: 打印所有配置项并退出</li>
+     * </ul>
+     *
+     * @param options 基础选项对象
+     * @return 添加了NameServer特定选项的Options对象
+     */
     public static Options buildCommandlineOptions(final Options options) {
+        // 配置文件选项
         Option opt = new Option("c", "configFile", true, "Name server config properties file");
         opt.setRequired(false);
         options.addOption(opt);
 
+        // 打印配置选项
         opt = new Option("p", "printConfigItem", false, "Print all config items");
         opt.setRequired(false);
         options.addOption(opt);
         return options;
     }
 
+    /**
+     * 获取配置属性集合
+     *
+     * @return 从配置文件加载的属性集合
+     */
     public static Properties getProperties() {
         return properties;
     }
